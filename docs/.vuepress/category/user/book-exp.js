02fc03f66300accbef94ecbb61aaa3328f478468
krircc <krircc@aliyun.com>
2019-06-20 15:41:51 +0800

    完成本书

--- docs/.vuepress/category/user/book-exp.js
+++ docs/.vuepress/category/user/book-exp.js
@@ -1,5 +1,17 @@
 exports.book_exp = (title) => {
     return [
-        ''
+        '',
+        '1. Getting Started',
+        '2. Programming a Guessing Game',
+        '3. Common Programming Concepts',
+        '4. Understanding Ownership',
+        '5. Using Structs to Structure Related Data',
+        '6. Enums and Pattern Matching',
+        '7. Modules',
+        '8. Common Collections',
+        '9. Error Handling',
+        '10. Generic Types, Traits, and Lifetimes',
+        '11. Writing Automated Tests',
+        '12. A Command Line Tool'
     ]
 }
